b17647da49f3f4341decbb2aee3189337804defd
abulhossainofficial <abulhossain.comc>
2024-07-03 20:23:21 +0600

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+// problem 1
+function main(input) {
+  /**
+   * Write JavaScript code from here
+   */
+  let a = Number(input.split(" ")[0]);
+  let b = Number(input.split(" ")[1]);
+
+  console.log(a % b);
+}
+
+// problem 2
+function main(input) {
+  /**
+   * Write JavaScript code from here
+   */
+  let a = Number(input.split(" ")[0]);
+  let b = Number(input.split(" ")[1]);
+
+  console.log(a % b);
+}
